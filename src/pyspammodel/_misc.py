0ff2d4316d397779cf06f5ad1cd5f965d7103b41
klklklo <68776358+klklklo@users.noreply.github.com>
2025-10-04 17:27:22 +0300

    Add recalculation functions

--- src/pyspammodel/_misc.py
+++ src/pyspammodel/_misc.py
@@ -1,5 +1,6 @@
 import functools
 import xarray as xr
+from numpy import array, arange
 from importlib_resources import files
 
 
@@ -15,3 +16,17 @@ def get_aero_spam_coeffs():
 
 def get_solar_spam_coeffs():
     return read_coeffs('_solar_spam_coeffs.nc').copy()
+
+
+def calc_diff_photon_flux(energy_flux):
+    h = 6.62607015e-34
+    c = 299792458
+    l = array(arange(0.5, 190.5, 1)) * 1e-9
+    return array([f / (h * c / l) for f, l in zip(energy_flux, l)])
+
+
+def calc_diff_energy_flux(photon_flux):
+    h = 6.62607015e-34
+    c = 299792458
+    l = array(arange(0.5, 190.5, 1)) * 1e-9
+    return array([p * (h * c / l) for p, l in zip(photon_flux, l)])
